51cdb1c4b85d44fd12a50be37266373d03fa196a
Iván E. Hernández T <iamieht@gmail.com>
2019-11-11 21:21:17 +0100

    Create areaOfCircle.py
    Week 0a - Practice Problems

---
+++ Introduction_to_interactive_programming_in_Python_PartI/Week0a/areaOfCircle.py
@@ -0,0 +1,15 @@
+# Compute the area of a circle, given the length of its radius.
+# The area of a circle is πr**2 where r is the radius of the circle. 
+# Write a Python statement that calculates and 
+# prints the area in square inches of a circle whose radius is 8 inches. 
+# Assume that the constant π=3.14
+###################################################
+# Circle area formula
+# Student should enter statement on the next line.
+print 3.14 * 8**2
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#200.96
