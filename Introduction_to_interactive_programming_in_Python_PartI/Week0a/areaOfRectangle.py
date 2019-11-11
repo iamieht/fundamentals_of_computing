f711db6e897c648bb67ca0571bc9e9640b5570fd
Iván E. Hernández T <iamieht@gmail.com>
2019-11-11 21:21:23 +0100

    Create areaOfRectangle.py

---
+++ Introduction_to_interactive_programming_in_Python_PartI/Week0a/areaOfRectangle.py
@@ -0,0 +1,16 @@
+# Compute the area of a rectangle, given its width and height.
+# The area of a rectangle is wh, where w and h are the lengths of its sides. 
+# Note that the multiplication operation is not shown explicitly in this formula. 
+# This is standard practice in mathematics, but not in programming. 
+# Write a Python statement that calculates and prints the area in square inches of a rectangle 
+# with sides of length 4 and 7 inches
+###################################################
+# Rectangle area formula
+# Student should enter statement on the next line.
+print 7 * 4
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#28
