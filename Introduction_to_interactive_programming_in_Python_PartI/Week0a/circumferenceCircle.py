fec9011cf6478628cd408b0a1cf6fe88ca24a6be
Iván E. Hernández T <iamieht@gmail.com>
2019-11-11 21:21:26 +0100

    Create circumferenceCircle.py

---
+++ Introduction_to_interactive_programming_in_Python_PartI/Week0a/circumferenceCircle.py
@@ -0,0 +1,15 @@
+# Compute the circumference of a circle, given the length of its radius.
+# The circumference of a circle is 2πr where r is the radius of the circle. 
+# Write a Python statement that calculates and prints the circumference in inches of a 
+# circle whose radius is 8 inches. Assume that the constant π=3.14
+###################################################
+# Circle circumference formula
+# Student should enter statement on the next line.
+print 2 * 3.14 * 8
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#50.24
