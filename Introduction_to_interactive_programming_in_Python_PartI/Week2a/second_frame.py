35f85925d1e4ae3e5ba1a811bf8914e048501dcd
Iván E. Hernández T <iamieht@gmail.com>
2019-11-16 14:48:17 +0100

    Create second_frame.py
    Week2a - Second frame

---
+++ Introduction_to_interactive_programming_in_Python_PartI/Week2a/second_frame.py
@@ -0,0 +1,22 @@
+# Open a frame
+
+###################################################
+# Open frame
+# Student should add code where relevant to the following.
+
+import simplegui 
+
+message = "My second frame!"
+
+# Handler for mouse click
+def click():
+    print message
+
+# Create a frame and assign callbacks to event handlers
+frame = simplegui.create_frame("My second frame!", 200, 100)
+# Assign callbacks to event handlers
+frame.add_button("Click me", click)
+
+# Start the frame animation
+frame.start()
+
