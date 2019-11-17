ef34fa120e7c8eca0720bab91e52f885fd621aec
Iván E. Hernández T <iamieht@gmail.com>
2019-11-17 11:52:42 +0100

    Create counter_with_buttons.py
    Week3b - Counter with Buttons

---
+++ Introduction_to_interactive_programming_in_Python_PartI/Week3b/counter_with_buttons.py
@@ -0,0 +1,37 @@
+# Counter with buttons
+
+###################################################
+# Student should add code where relevant to the following.
+
+import simplegui 
+
+counter = 0
+
+# Timer handler
+def tick():
+    global counter
+    print counter
+    counter += 1
+    
+# Event handlers for buttons  
+def start():
+    timer.start()
+    
+def stop():
+    timer.stop()
+
+def reset():
+    global counter
+    counter = 0
+
+        
+# Create frame and timer
+frame = simplegui.create_frame("Counter with buttons", 200, 200)
+timer = simplegui.create_timer(1000, tick)
+frame.add_button("Start", start)
+frame.add_button("Stop", stop)
+frame.add_button("Reset", reset)
+
+frame.start()
+# Start timer
+#timer.start()
