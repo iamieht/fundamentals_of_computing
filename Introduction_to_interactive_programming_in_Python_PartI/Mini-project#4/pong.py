0f0e71e3824610a23359b99278da6510b35ab719
Iván E. Hernández T <iamieht@gmail.com>
2019-11-20 19:18:37 +0100

    Update pong.py
    Pong -> Final

--- Introduction_to_interactive_programming_in_Python_PartI/Mini-project#4/pong.py
+++ Introduction_to_interactive_programming_in_Python_PartI/Mini-project#4/pong.py
@@ -17,11 +17,13 @@ paddle1_pos = (HEIGHT / 2)
 paddle2_pos = (HEIGHT / 2)
 paddle1_vel = 0
 paddle2_vel = 0
+score1 = 0
+score2 = 0
 
 # initialize ball_pos and ball_vel for new bal in middle of table
 # if direction is RIGHT, the ball's velocity is upper right, else upper left
 def spawn_ball(direction):
-    global ball_pos, ball_vel # these are vectors stored as lists
+    global ball_pos, ball_vel
     ball_pos = [WIDTH / 2, HEIGHT / 2]
     ball_vel = [(random.randrange(120/60, 240/60)), (random.randrange(60/60, 180/60)) ] # pixels per second
   
@@ -37,6 +39,8 @@ def spawn_ball(direction):
 def new_game():
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
     global score1, score2  # these are ints
+    score1 = 0
+    score2 = 0
     spawn_ball("RIGHT")
     
 def draw(canvas):
@@ -60,34 +64,72 @@ def draw(canvas):
     
     # draw ball
     canvas.draw_circle(ball_pos, BALL_RADIUS, 5, 'White', 'White')
-    # update paddle's vertical position, keep paddle on the screen
     
+    # update paddle's vertical position, keep paddle on the screen
+    if paddle1_pos + paddle1_vel >= HALF_PAD_HEIGHT and paddle1_pos + paddle1_vel <= HEIGHT - HALF_PAD_HEIGHT:
+        paddle1_pos += paddle1_vel
+         
+    if paddle2_pos + paddle2_vel >= HALF_PAD_HEIGHT and paddle2_pos + paddle2_vel <= HEIGHT - HALF_PAD_HEIGHT:
+        paddle2_pos += paddle2_vel
+        
     # draw paddles
+    canvas.draw_line([HALF_PAD_WIDTH, paddle1_pos - HALF_PAD_HEIGHT], [HALF_PAD_WIDTH, paddle1_pos + HALF_PAD_HEIGHT], PAD_WIDTH, 'White')
+    canvas.draw_line([WIDTH - HALF_PAD_WIDTH, paddle2_pos - HALF_PAD_HEIGHT], [WIDTH - HALF_PAD_WIDTH, paddle2_pos + HALF_PAD_HEIGHT], PAD_WIDTH, 'White')
     
     # determine whether paddle and ball collide
     if ball_pos[0] <= BALL_RADIUS + PAD_WIDTH:
         if paddle1_pos - HALF_PAD_HEIGHT <= ball_pos[1] and ball_pos[1] <= paddle1_pos + HALF_PAD_HEIGHT:
             ball_vel[0] = -ball_vel[0]
+            
+            # Increase ball speed by 10%
+            ball_vel[0] += ball_vel[0] * 0.10
+            ball_vel[1] += ball_vel[1] * 0.10
         
         else:
+            score2 += 1
             spawn_ball("RIGHT")
             
     elif ball_pos[0] >= (WIDTH - 1) - BALL_RADIUS - PAD_WIDTH:
         if paddle2_pos - HALF_PAD_HEIGHT <= ball_pos[1] and ball_pos[1] <= paddle2_pos + HALF_PAD_HEIGHT:
             ball_vel[0] = -ball_vel[0]
+            
+            # Increase ball speed by 10%
+            ball_vel[0] += ball_vel[0] * 0.10
+            ball_vel[1] += ball_vel[1] * 0.10
         
         else:
+            score1 += 1
             spawn_ball("LEFT")
     
     # draw scores
+    canvas.draw_text(str(score1),[150,50],40,"White", 'sans-serif');
+    canvas.draw_text(str(score2),[450,50],40,"White", 'sans-serif');
         
 def keydown(key):
     global paddle1_vel, paddle2_vel
-    pass
+    acc = 3
+    if key == simplegui.KEY_MAP["w"]:
+        paddle1_vel -= acc
+    elif key == simplegui.KEY_MAP["s"]:
+        paddle1_vel += acc
+        
+    if key == simplegui.KEY_MAP["up"]:
+        paddle2_vel -= acc
+    elif key == simplegui.KEY_MAP["down"]:
+        paddle2_vel += acc
    
 def keyup(key):
     global paddle1_vel, paddle2_vel
-    pass
+    acc = 3
+    if key == simplegui.KEY_MAP["w"]:
+        paddle1_vel += acc
+    elif key == simplegui.KEY_MAP["s"]:
+        paddle1_vel -= acc
+        
+    if key == simplegui.KEY_MAP["up"]:
+        paddle2_vel += acc
+    elif key == simplegui.KEY_MAP["down"]:
+        paddle2_vel -= acc
 
 
 # create frame
@@ -95,6 +137,7 @@ frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
 frame.set_keyup_handler(keyup)
+frame.add_button("Restart",new_game,100)
 
 
 # start frame
