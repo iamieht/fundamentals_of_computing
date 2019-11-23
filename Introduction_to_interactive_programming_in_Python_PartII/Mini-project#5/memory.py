4230f2474b2710d8ffea9fe7999d12ba65465892
Iván E. Hernández T <iamieht@gmail.com>
2019-11-23 16:09:37 +0100

    Update memory.py
    Mini-project#5: Memory final

--- Introduction_to_interactive_programming_in_Python_PartII/Mini-project#5/memory.py
+++ Introduction_to_interactive_programming_in_Python_PartII/Mini-project#5/memory.py
@@ -3,31 +3,81 @@
 import simplegui
 import random
 
-# global variables
-cardsDeck = list(range(0,8)) + list(range(0,8))
-cardsDeckExposed = 16 * [True]
-cardSize = [50, 100]
+# Global Variables
+turns = 0 
 
 # helper function to initialize globals
 def new_game():
-    global cardsDeck, cardsDeckExposed
-    cardsDeck = list(range(0,8)) + list(range(0,8))
-    cardsDeckExposed = 16 * [True]
-    random.shuffle(cardsDeck)
+    '''
+    Start of the game
+    '''
+    global memory_deck, exposed, state, turns
+    state = 0 # -> Number of cards exposed
+    turns = 0
+    label.set_text("Turns = " + str(turns))
+    memory_deck = range(0,8) + range(0,8)
+    random.shuffle(memory_deck)
+    exposed = [False] * 16 # List: True-> Card Exposed / False-> Not exposed
+
 
      
 # define event handlers
 def mouseclick(pos):
-    # add game state logic here
-    pass
+    '''
+    Main Game Logic
+    '''
+    global exposed, state, card1_idx, card2_idx, turns
+
+    # index of the clicked card
+    idx = pos[0] // 50
     
-                        
+    # flip cards depending on the state
+    if state == 0:
+        
+        exposed[idx] = True
+        card1_idx = idx
+        state = 1
+        
+    elif state == 1:
+        
+        if not exposed[idx]:
+            exposed[idx] = True
+            card2_idx = idx
+            turns += 1 # Increase the turns when the first card is flipped
+            
+            state = 2        
+            label.set_text("Turns = " + str(turns))
+              
+    elif state == 2:
+        
+        if not exposed[idx]:
+            exposed[idx] = True
+            
+            if not memory_deck[card1_idx] == memory_deck[card2_idx]:
+                exposed[card1_idx] = False
+                exposed[card2_idx] = False
+                
+            card1_idx = idx
+                                        
+            state = 1 
+    
+                            
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
-    for idx in range(len(cardsDeck)):
-        if cardsDeckExposed[idx]:
-            canvas.draw_text(str(cardsDeck[idx]), [(idx * cardSize[0] + cardSize[0] / 2), 60], 32, "White")
-            
+    global exposed, turns
+
+    for card in range(1, 17):
+        if exposed[card - 1]:
+            # Horizontal location
+            horz = (card * 50) - 35            
+            canvas.draw_text(str(memory_deck[card - 1]), [horz, 65], 48, "White")
+        else:
+            # Rectangle points
+            a = (card * 50) - 50
+            b = 0
+            c = card * 50
+            d = 100
+            canvas.draw_polygon([(a, b), (a, d), (c, d), (c, b)], 2, "Black", "Green")
         
 
 
@@ -35,6 +85,7 @@ def draw(canvas):
 frame = simplegui.create_frame("Memory", 800, 100)
 frame.add_button("Reset", new_game)
 label = frame.add_label("Turns = 0")
+
 
 # register event handlers
 frame.set_mouseclick_handler(mouseclick)
