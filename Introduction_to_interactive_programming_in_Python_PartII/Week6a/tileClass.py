c9ed8b4951e91fe541ac7c3dddb6708a1c1204f4
Iván E. Hernández T <iamieht@gmail.com>
2019-11-24 08:30:26 +0100

    Update tileClass.py
    add is_selected() and mouseclick handler

--- Introduction_to_interactive_programming_in_Python_PartII/Week6a/tileClass.py
+++ Introduction_to_interactive_programming_in_Python_PartII/Week6a/tileClass.py
@@ -33,7 +33,19 @@ class Tile(object):
         else:
             tile_corners = (loc, [loc[0] + TILE_WIDTH, loc[1]], [loc[0] + TILE_WIDTH, loc[1] - TILE_HEIGHT], [loc[0], loc[1] - TILE_HEIGHT])
             canvas.draw_polygon(tile_corners, 1, "Red", "Green")
+    
+    def is_selected(self, pos):
+        inside_hor = self.location[0] <= pos[0] < self.location[0] + TILE_WIDTH
+        inside_vert = self.location[1] - TILE_HEIGHT <= pos[1] <= self.location[1]
+        return  inside_hor and inside_vert 
 
+
+# define event handlers
+def mouseclick(pos):
+    if tile1.is_selected(pos):
+        tile1.hide_tile()
+    if tile2.is_selected(pos):
+        tile2.expose_tile()
 
 # draw handler
 def draw(canvas):
@@ -43,6 +55,7 @@ def draw(canvas):
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 2 * TILE_WIDTH, TILE_HEIGHT)
 frame.set_draw_handler(draw)
+frame.set_mouseclick_handler(mouseclick)
 
 # create two tiles  
 tile1 = Tile(3, True, [0, TILE_HEIGHT])
