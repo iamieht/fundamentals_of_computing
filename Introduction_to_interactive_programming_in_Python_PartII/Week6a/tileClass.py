3a82471e63e4ab5be02b430e1efaff4f20192109
Iván E. Hernández T <iamieht@gmail.com>
2019-11-24 08:19:40 +0100

    Update tileClass.py
    Added is_exposed(), expose_tile() and hide_tile()

--- Introduction_to_interactive_programming_in_Python_PartII/Week6a/tileClass.py
+++ Introduction_to_interactive_programming_in_Python_PartII/Week6a/tileClass.py
@@ -1,10 +1,22 @@
 class Tile(object):
-    def __init__(self, num):
+    def __init__(self, num, exposed):
         self.number = num
+        self.exposed = exposed
 
     def get_number(self):
         return self.number
+    
+    def is_exposed(self):
+        return self.exposed
+    
+    def expose_tile(self):
+        self.exposed = True
+    
+    def hide_tile(self):
+        self.exposed = False
+
+    
 
 
-my_tile = Tile(3)
-your_tile = Tile(4)
+my_tile = Tile(3, True)
+your_tile = Tile(4, False)
