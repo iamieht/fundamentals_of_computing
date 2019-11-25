7011e9de8cbd3bc2d898fc81b5b69a8e0958c089
Iván E. Hernández T <iamieht@gmail.com>
2019-11-25 08:31:52 +0100

    Update personClass.py
    Add average_age function

--- Introduction_to_interactive_programming_in_Python_PartII/Week6a/personClass.py
+++ Introduction_to_interactive_programming_in_Python_PartII/Week6a/personClass.py
@@ -13,8 +13,29 @@ class Person(object):
     def __str__(self):
         return "Person's name is " + self.full_name() + ". " + "Their birth year is " + str(self.birth_year)
 
+# Test Person Class
+# me = Person("Ivan", "Hernandez", 1977)
+# print me.full_name()
+# print me
+# print me.age(2019)
 
-me = Person("Ivan", "Hernandez", 1977)
-print me.full_name()
-print me
-print me.age(2019)
+def average_age(listOfPersons, currentYear):
+    totalAges = 0
+
+    for person in listOfPersons:
+        totalAges += person.age(currentYear)
+    
+    return totalAges / len(listOfPersons)
+
+
+# Test average_age
+ivan = Person("Ivan", "Hernandez", 1977)
+maria = Person("Maria", "Antonieta", 1986)
+josefina = Person("Jose", "Fina", 1990)
+rosa = Person("Rosa", "Melo", 2000)
+jessica = Person("Jessica", "Jones", 1999)
+cojo = Person("Co", "Jones", 2001)
+
+listOfPersons = [ivan, maria, josefina, rosa, jessica, cojo]
+
+print average_age(listOfPersons, 2019)
