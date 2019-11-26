6df3a02853c436d8e03179d518a6f66c073b0126
Iván E. Hernández T <iamieht@gmail.com>
2019-11-26 08:17:42 +0100

    Update studentClass.py
    Assign function

--- Introduction_to_interactive_programming_in_Python_PartII/Week6a/studentClass.py
+++ Introduction_to_interactive_programming_in_Python_PartII/Week6a/studentClass.py
@@ -31,17 +31,55 @@ class Student(Person):
     def add_project(self, project_name):
         self.projects.append(project_name)
 
+
+def assign(listOfStudents, studentName, password, project_name):
+    
+    for student in listOfStudents:
+        if student.get_name() == studentName and student.check_password(password):
+            projects = student.get_projects()
+            if project_name not in projects:
+                student.add_project(project_name)
+
+
+
+
 # Testing code
 
-joe_person = Person("Joe", "Warren", 52)
-joe_student = Student(joe_person, "TopSecret")
+# joe_person = Person("Joe", "Warren", 52)
+# joe_student = Student(joe_person, "TopSecret")
 
-print joe_student.get_name()
-print joe_student.check_password("qwert")
-print joe_student.check_password("TopSecret")
+# print joe_student.get_name()
+# print joe_student.check_password("qwert")
+# print joe_student.check_password("TopSecret")
 
-print joe_student.get_projects()
-joe_student.add_project("Create practice exercises")
-print joe_student.get_projects()
-joe_student.add_project("Implement Minecraft")
-print joe_student.get_projects()
+# print joe_student.get_projects()
+# joe_student.add_project("Create practice exercises")
+# print joe_student.get_projects()
+# joe_student.add_project("Implement Minecraft")
+# print joe_student.get_projects()
+
+# Testing code assign function
+# create some Student objects using Person object
+joe = Student(Person("Joe", "Warren", 52), "TopSecret")
+joe.add_project("Create practice exercises")
+joe.add_project("Implement Minecraft")
+
+scott = Student(Person("Scott", "Rixner", 29), "CodeSkulptor")
+scott.add_project("Beat Joe at RiceRocks")
+
+john = Student(Person("John", "Greiner", 47), "outdoors")
+
+
+# create a list of students
+profs = [joe, scott, john]
+
+# test assign
+print joe.get_projects()
+assign(profs, "Joe Warren", "TopSecret", "Practice RiceRocks")
+print joe.get_projects()
+
+print john.get_projects()
+assign(profs, "John Greiner", "OUTDOORS", "Work on reflexes")
+print john.get_projects()
+assign(profs, "John Greiner", "outdoors", "Work on reflexes")
+print john.get_projects()
